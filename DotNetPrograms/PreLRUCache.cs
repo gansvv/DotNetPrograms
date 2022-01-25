4bcddb1e5bf66234aca3e5115d17f6013c0a3615
Ganesh Viswanathan <gansvv@gmail.com>
2022-01-25 06:09:12 -0800

    Added LRU Cache

---
+++ DotNetPrograms/PreLRUCache.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNetPrograms
+{
+    public class PreLRUCache
+    {
+        private LinkedList<KeyValuePair<int, int>> list;
+        private Dictionary<int, LinkedListNode<KeyValuePair<int, int>>> map;
+        private int capacity;
+        public PreLRUCache(int capacity)
+        {
+            this.capacity = capacity;
+            list = new LinkedList<KeyValuePair<int, int>>();
+            map = new Dictionary<int, LinkedListNode<KeyValuePair<int, int>>>();
+        }
+
+        public int Get(int key)
+        {
+            if (!map.ContainsKey(key)) return -1;
+            var node = map[key];
+            list.Remove(node);
+            map[key] = list.AddFirst(node.Value);
+            return node.Value.Value;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (map.ContainsKey(key))
+            {
+                var node = map[key];
+                list.Remove(node);
+                map[key] = list.AddFirst(new KeyValuePair<int, int>(key, value));
+            }
+            else
+            {
+                if (list.Count >= this.capacity)
+                {
+                    map.Remove(list.Last().Key);
+                    list.RemoveLast();
+                }
+                map[key] = list.AddFirst(new KeyValuePair<int, int>(key, value));
+            }
+        }
+    }
+}
--- DotNetPrograms/Program.cs
+++ DotNetPrograms/Program.cs
@@ -10,6 +10,18 @@ namespace DotNetPrograms
 
             LevelOrderTreeTraversal.Run();
 
+            Console.WriteLine("LRU Cache:");
+
+            LRUCache<int, int> cache = new LRUCache<int, int>(3);
+            cache.Add(1, 10);
+            cache.Add(2, 20);
+            cache.Add(3, 30);
+            Console.WriteLine("Get valid number from cache: {0}", cache.Get(2));
+            cache.Add(4, 40);
+            Console.WriteLine("TryGet using invalidated key from cache: {0}", cache.Get(1));
+            Console.WriteLine("Get valid number from cache: {0}", cache.Get(4));
+            Console.WriteLine("Get valid number from cache: {0}", cache.Get(2));
+
             Console.WriteLine("\nRun complete.");
         }
     }
